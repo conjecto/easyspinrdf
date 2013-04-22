82f3c365e8787c8e438ae811f886c61d1205174f
Blaise de Carné <bdecarne@gmail.com>
2013-04-22 12:11:18 +0200

    fix utils

--- lib/EasySpinRdf/Utils.php
+++ lib/EasySpinRdf/Utils.php
@@ -48,11 +48,6 @@ class EasySpinRdf_Utils
         EasyRdf_TypeMapper::set('sp:Describe', 'EasySpinRdf_Query_Describe');
         EasyRdf_TypeMapper::set('sp:Construct', 'EasySpinRdf_Query_Construct');
 
-        EasyRdf_TypeMapper::set('sp:Ask', 'EasySpinRdf_Query_Ask');
-        EasyRdf_TypeMapper::set('sp:Select', 'EasySpinRdf_Query_Select');
-        EasyRdf_TypeMapper::set('sp:Describe', 'EasySpinRdf_Query_Describe');
-        EasyRdf_TypeMapper::set('sp:Construct', 'EasySpinRdf_Query_Construct');
-
         // Elements
         EasyRdf_TypeMapper::set('sp:SubQuery', 'EasySpinRdf_Element_SubQuery');
         EasyRdf_TypeMapper::set('sp:NamedGraph', 'EasySpinRdf_Element_NamedGraph');
